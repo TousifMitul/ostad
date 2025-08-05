84bfa67cec180c58ed4b1530c1c13b02c0fad870
Tousif Robbani <168193378+TousifMitul@users.noreply.github.com>
2025-08-06 00:38:36 +0600

    Add files via upload

---
+++ ostad_assignment_1/test/ostad_assignment_1_test.dart
@@ -0,0 +1,16 @@
+import 'package:ostad_assignment_1/ostad_assignment_1.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    final awesome = Awesome();
+
+    setUp(() {
+      // Additional setup goes here.
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
